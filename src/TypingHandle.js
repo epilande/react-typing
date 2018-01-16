98b4a4607f942de650eaae91fb939364b75bc1c5
Emmanuel Pilande <epilande@gmail.com>
2018-01-16 06:19:55 -0800

    :100: Fire callback when TypingHandle is done

--- src/TypingHandle.js
+++ src/TypingHandle.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TypingHandle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      completed: false,
+    };
+  }
+
   componentDidMount() {
     this.props.typingHandler({ ...this.props, type: 'TypingHandle' });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!this.state.completed && nextProps.children === this.props.children) {
+      this.setState({ completed: true });
+      this.props.typingHandler({ ...this.props, type: 'TypingHandle', completed: true });
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.children !== this.props.children) {
+      return true;
+    }
+    return false;
   }
 
   render() {
@@ -15,8 +37,13 @@ class TypingHandle extends Component {
   }
 }
 
-TypingHandle.defaultProps = {};
+TypingHandle.defaultProps = {
+  typingHandler: () => {},
+};
 
-TypingHandle.propTypes = {};
+TypingHandle.propTypes = {
+  typingHandler: PropTypes.func,
+  children: PropTypes.node.isRequired,
+};
 
 export default TypingHandle;
