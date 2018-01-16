11723c9c7377c407b9578beb5d281d7f411c5146
Emmanuel Pilande <epilande@gmail.com>
2018-01-16 06:36:27 -0800

    :art: Clean up

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -6,8 +6,9 @@ const colors = {
   background: '#F9F6F2',
   primary: '#E55A32',
   text: '#1B4B61',
-}
+};
 
+// eslint-disable-next-line
 injectGlobal`
   body {
     background: ${colors.background};
@@ -19,12 +20,12 @@ injectGlobal`
   p {
     font-size: 1.125rem;
   }
-`
+`;
 
-const Title = styled.h1`
+const Title = styled.h3`
   margin-top: 12.5rem;
   color: ${colors.primary};
-  font-size: 3.125rem;
+  font-size: 2.25rem;
   text-transform: uppercase;
   letter-spacing: 0.3rem;
 `;
@@ -33,7 +34,7 @@ const Invert = styled.span`
   padding: 0.5rem 1.25rem;
   background: ${colors.primary};
   color: ${colors.background};
-`
+`;
 
 export default () =>
   <div>
